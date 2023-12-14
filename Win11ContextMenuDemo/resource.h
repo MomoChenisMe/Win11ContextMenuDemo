4cd53cc31b19e53e43fde8be8bdea713929c42ed
MomoChenIsMe <momochenisme@gmail.com>
2023-12-14 16:46:19 +0800

    Add multi language resource

---
+++ Win11ContextMenuDemo/resource.h
@@ -0,0 +1,18 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 產生的 Include 檔案。
+// 由 Win11ContextMenuDemo.rc 使用
+//
+#define ID_WIN11CONTEXTMENUDEMO_DEFAULT                101
+#define ID_WIN11CONTEXTMENUDEMO_MAIN                   102
+#define ID_WIN11CONTEXTMENUDEMO_SUB1                   103
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
